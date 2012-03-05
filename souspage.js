69f8576642cf218dc7007dc86eb1bab0b1d3349a
Loïc Raucy <loic.raucy@gmail.com>
2012-03-05 21:25:50 +0100

    Added last changes for matching with current site version

--- souspage.js
+++ souspage.js
@@ -24,6 +24,15 @@ $(document).ready(function() {
 		return false;
 	});
 
+	$(".sous_cadre_liens .sous_menu").delegate("a", "click", function() {
+		var classOfContent = $(this).attr("class");
+		$(".sous_contenu").removeClass("contenu5A");
+		$(".sous_contenu").removeClass("contenu5B");
+		$(".sous_contenu").addClass(classOfContent);
+		$(".sous_cadre_liens .sous_contenu").load(this.href);
+		return false;
+	});
+
 	$(".sous_menu_list").delegate("a", "click", function() {
 		$(".sous_menu_list div.selected").removeClass("selected");
 		$(this.parentNode).addClass("selected");
@@ -33,4 +42,5 @@ $(document).ready(function() {
 	$(".sous_contenu_list").load("stylisme/page1.html");
 	$(".sous_cadre_graphisme .sous_contenu").load("graphisme/comm.html");
 	$(".sous_cadre_illus .sous_contenu").load("illus/silhouettes.html");
+	$(".sous_cadre_liens .sous_contenu").load("liens/confiance.html");
 });
---
+++ soussouspage.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+	$(".list_4_images").delegate("a", "click", function() {
+		return false;
+	});
+	$(".list_3_images").delegate("a", "click", function() {
+		return false;
+	});
+});
